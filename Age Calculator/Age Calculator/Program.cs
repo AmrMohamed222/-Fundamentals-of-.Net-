61fad54617a59ebcd422704df0bfab3ce1f5291c
Amr Mohamed <111697430+AmrMohamed222@users.noreply.github.com>
2024-10-21 23:55:15 +0300

    Add files via upload

---
+++ Age Calculator/Age Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Age_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your birth year: ");
+            int x = int.Parse(Console.ReadLine()), Age, current_year = 2024;
+            Age = current_year - x;
+            Console.WriteLine("You are " + Age + " years old.");
+        }
+    }
+}
