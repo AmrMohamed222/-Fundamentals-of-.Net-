225a1fcc98b83a6fe827d2eb0c97192e11837edd
Amr Mohamed <amrm91432@outlook.com>
2024-10-22 00:21:53 +0300

    Add project files.

---
+++ Temperature Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the temperature in celsius");
+            float C = float.Parse(Console.ReadLine()), F;
+            F = C * 9/5 + 32;
+            Console.WriteLine(C + " degrees Celsius is " + F + " degrees Fahrenheit");
+        }
+    }
+}
